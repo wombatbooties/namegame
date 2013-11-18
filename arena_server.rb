384d740870f615149659381099a15a0d7f362a54
wombatbooties <josh.broderson@gmail.com>
2013-11-17 21:13:24 -0900

    added count and a cheesy fighting sim

--- arena_server.rb
+++ arena_server.rb
@@ -4,16 +4,34 @@ class ArenaServer
   def initialize
     @pen = []
     @name = ''
+    @count = 0
   end
+
+  def count
+    return @count
+  end
+
+  def heroes
+    return @pen
+  end
+
   def cage(name)
     @pen.push(name)
     if @pen.size <= 1
       puts 'waiting for another fighter.'
     else
-      puts @pen.pop + ' fights ' + @pen.pop
+      @count += 1
+      @heroA = @pen.pop
+      @heroB = @pen.pop
+      if rand(100) >= 50
+        @pen.push(@heroA)
+      else
+	@pen.push(@heroB)
+      end
     end
   end
 end
+
 server = ArenaServer.new
 DRb.start_service('druby://localhost:9000', server)
 DRb.thread.join
